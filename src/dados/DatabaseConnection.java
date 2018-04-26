0238bd187d85b7780f47ebe18b6552d74e0d2a09
Hugo Lucas <hddlucas94@gmail.com>
2018-04-26 18:35:39 +0100

    Initial commit

---
+++ src/dados/DatabaseConnection.java
@@ -0,0 +1,79 @@
+package dados;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    protected   Connection connection;
+    private String username = "root";
+    private String password = "";
+    private String url = "jdbc:h2:~/ExcelSaga";
+
+    private DatabaseConnection()   {
+
+        try {
+            String script=this.getDBScript("files/script.sql");
+            Class.forName("org.h2.Driver");
+
+            connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            statement.executeUpdate(script);
+
+        } catch (ClassNotFoundException  | IOException | SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null) {
+            connection.close();
+            connection = null;
+        }
+    }
+
+    public static DatabaseConnection getInstance() throws Exception {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
+
+
+    private String getDBScript(String file) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        BufferedReader reader = new BufferedReader(new FileReader(classLoader.getResource(file).getPath()));
+        String         line = null;
+        StringBuilder  stringBuilder = new StringBuilder();
+        String         ls = System.getProperty("line.separator");
+
+        try {
+            while((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            return stringBuilder.toString();
+        } finally {
+            reader.close();
+        }
+    }
+
+
+}
---
+++ src/dados/UsersController.java
@@ -0,0 +1,53 @@
+package dados;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersController {
+
+
+    //get list of stored users in database
+    public static List<String> getUsersList() throws Exception {
+        List<String> users = new ArrayList<>();
+
+        try(Statement statement = DatabaseConnection.getInstance().getConnection().createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM  USER")) {
+            while (rs.next()) {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        return users;
+    }
+
+
+    //check if user exists on database
+    public static boolean checkIfUserExists(String name) throws Exception {
+
+        try(PreparedStatement statement = DatabaseConnection.getInstance()
+                .getConnection().prepareStatement("SELECT ID FROM USER WHERE LOWER(NAME) =?")) {
+            statement.setString(1, name.toLowerCase());
+            try(ResultSet rs = statement.executeQuery()) {
+                if( rs.next() ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+
+    //check if user exists on database
+    public static void create(String name) throws Exception {
+        try(PreparedStatement statement = DatabaseConnection.getInstance()
+                .getConnection().prepareStatement("INSERT INTO USER (NAME) VALUES (?)")) {
+            statement.setString(1, name);
+            statement.executeUpdate();
+
+        }
+    }
+
+
+}
