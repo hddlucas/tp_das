7da29b1e9ae4cbcc921cf80ed7583aec24a1db5a
Hugo Lucas <hddlucas94@gmail.com>
2018-07-22 17:09:13 +0100

    implemented number formula

--- src/bll/formulas/FormulaNumber.java
+++ src/bll/formulas/FormulaNumber.java
@@ -5,20 +5,41 @@
  */
 package bll.formulas;
 
+import static gui.ExcelSaga.excelSagaTableModel;
+
 /**
  *
  * @author hdlucas
  */
 public class FormulaNumber extends Formula{
 
-    @Override
-    public String getFormulaResult(String[] params){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     @Override
+    public String getFormulaResult(String[] params) {
+        try {
+            validateNumberOfParams(params);
+
+            columnName = params[0].replaceAll("\\d", "");
+            columnIndex= excelSagaTableModel.findColumn(columnName);
+            rowIndex = Integer.parseInt( params[0].replaceAll("\\D+",""));
+
+            Double number=Double.parseDouble(excelSagaTableModel.getValueAt(rowIndex-1, columnIndex).toString());
+            newCellValue=number.toString();
+            
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Out of Bounds Exc. Expected Number of Parameters is 1");
+            return invalidFormula;
+        } catch (Exception e) {
+            return invalidFormula;
+        }
+        
+        return newCellValue;
     }
 
     @Override
-    public void validateNumberOfParams(String[] params) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void validateNumberOfParams(String[] params) throws ArrayIndexOutOfBoundsException {
+        if (params.length != 1) {
+            throw new ArrayIndexOutOfBoundsException("Out of Bounds Exc. Size is 1");
+        }
     }
     
 }
